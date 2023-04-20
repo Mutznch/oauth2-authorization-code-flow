1f92afc0a798de39117499d7d3be1538f27e19bd
Mutznch <110611081+Mutznch@users.noreply.github.com>
2023-04-20 16:33:02 -0300

    Delete code directory

--- code/_bin/dotenvsetup.mjs
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-import dotenvExpand from 'dotenv-expand'
-
-dotenv.config()
-const result = dotenvExpand.expand(
-    { ignoreProcessEnv: false, parsed:process.env }
-)
-
-export default result.parsed
